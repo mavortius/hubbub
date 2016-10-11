7085bd69be06365c4ad9ae9909f45e93d2766bdf
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2016-10-11 19:05:08 -0300

    Added PostController and initial unit tests.

--- grails-app/controllers/com/grailsinaction/hubbub/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/hubbub/PostController.groovy
@@ -1,6 +1,15 @@
 package com.grailsinaction.hubbub
 
 class PostController {
-
     static scaffold = Post
+
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+
+        if(user) {
+            [user:user]
+        } else {
+            response.sendError(404)
+        }
+    }
 }
