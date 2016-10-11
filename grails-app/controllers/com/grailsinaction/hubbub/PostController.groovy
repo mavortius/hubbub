36a204dc54101e76193a64dc34492f9caa6b3b9d
Marcelo Martins <m.a.martins@live.com>
2016-10-11 14:51:33 -0300

    Removed test method.

--- grails-app/controllers/com/grailsinaction/hubbub/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/hubbub/PostController.groovy
@@ -3,10 +3,4 @@ package com.grailsinaction.hubbub
 class PostController {
 
     static scaffold = Post
-
-    def index() {
-        def post = new Post(content: "test")
-        def posts = Post.findAll(post, max: 5)
-    }
-
 }
