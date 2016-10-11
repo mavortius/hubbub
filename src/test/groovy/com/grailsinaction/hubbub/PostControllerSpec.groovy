8e8f657f3984ea57671e1049e523d377c7e5cf62
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2016-10-11 19:06:28 -0300

    Added PostController and initial unit tests.

---
+++ src/test/groovy/com/grailsinaction/hubbub/PostControllerSpec.groovy
@@ -0,0 +1,40 @@
+package com.grailsinaction.hubbub
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.*
+
+@TestFor(PostController)
+@Mock([User, Post])
+class PostControllerSpec extends Specification {
+    def "Get a users timeline given their id"() {
+        given: "A user with posts in the db"
+        User chuck = new User(
+                loginId: "chuck_norris",
+                password: "password")
+        chuck.addToPosts(new Post(content: "A first post"))
+        chuck.addToPosts(new Post(content: "A second post"))
+        chuck.save(failOnError: true)
+
+        and: "A loginId parameter"
+        params.id = chuck.loginId
+
+        when: "the timeline is invoked"
+        def model = controller.timeline()
+
+        then: "the user is in returned model"
+        model.user.loginId == "chuck_norris"
+        model.user.posts.size() == 2
+    }
+
+    def "Check that non-existent users are handled with an error"() {
+        given: "the id of a non-existent user"
+        params.id = "this-user-id-does-not-exist"
+
+        when: "the timeline is invoked"
+        controller.timeline()
+
+        then: "a 404 is sent to the browser"
+        response.status == 404
+    }
+}
