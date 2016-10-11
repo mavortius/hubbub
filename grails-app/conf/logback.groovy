07ce49196a71289ed0e8379ad08df2c7c0776f99
Marcelo Martins <m.a.martins@live.com>
2016-10-11 09:54:01 -0300

    Added settings to log SQL clauses and parameters.

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -8,6 +8,9 @@ appender('STDOUT', ConsoleAppender) {
     }
 }
 
+logger 'org.hibernate.type.descriptor.sql.BasicBinder', TRACE, ['STDOUT']
+logger 'org.hibernate.SQL', TRACE, ['STDOUT']
+
 def targetDir = BuildSettings.TARGET_DIR
 if (Environment.isDevelopmentMode() && targetDir != null) {
     appender("FULL_STACKTRACE", FileAppender) {
